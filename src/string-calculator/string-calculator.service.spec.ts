575f4a9068aaaf2ad7d71e8d81911cc15ea65cbc
Rishabh Pandey <rishabh.pandey@trio.so>
2025-03-19 19:18:22 +0530

    feat(service/test): added basic tests & service method

--- src/string-calculator/string-calculator.service.spec.ts
+++ src/string-calculator/string-calculator.service.spec.ts
@@ -15,4 +15,21 @@ describe('StringCalculatorService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return 0 for an empty string', () => {
+    expect(service.add("")).toBe(0);
+  });
+
+  it('should return the number itself if only one number is provided', () => {
+    expect(service.add("1")).toBe(1);
+  });
+
+  it('should return the sum of two numbers', () => {
+    expect(service.add("1,5")).toBe(6);
+  });
+
+  it('should handle multiple numbers addition', () => {
+    expect(service.add("1,2,3,4")).toBe(10);
+  });
+
 });
--- src/string-calculator/string-calculator.service.ts
+++ src/string-calculator/string-calculator.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StringCalculatorService {}
+export class StringCalculatorService {
+    add(numbers: string): number {
+        if (!numbers) return 0;
+    
+        let delimiter = /,/; //comma separated
+        if (numbers.startsWith("//")) {
+          const parts = numbers.split("\n");
+          delimiter = new RegExp(parts[0].substring(2));
+          numbers = parts[1];
+        }
+    
+        const numArray = numbers.split(delimiter).map(Number);
+    
+        return numArray.reduce((sum, num) => sum + num, 0);
+      }
+}
