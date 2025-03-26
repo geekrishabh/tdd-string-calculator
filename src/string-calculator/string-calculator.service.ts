6536e0d1681bbdb356ea33a5e07dbcc718d6af04
Rishabh Pandey <geekrishabh@gmail.com>
2025-03-26 16:52:58 +0530

    chore(*): refactor based on solid design

--- src/string-calculator/string-calculator.service.ts
+++ src/string-calculator/string-calculator.service.ts
@@ -5,27 +5,41 @@ export class StringCalculatorService {
     add(numbers: string): number {
         if (!numbers) return 0;
     
-        let delimiter = /,|\n/;
-        if (numbers.startsWith("//")) {
-          const parts = numbers.split("\n");
-          delimiter = new RegExp(parts[0].substring(2));
-          numbers = parts[1];
-        }
+       const delimiter = this.extractDelimiter(numbers)
+       const sanitaizeNumber = this.extractNumber(numbers, delimiter)
     
+        const postivenumber = this.validateNegativeNumber(sanitaizeNumber);
+    
+        return postivenumber.reduce((sum, num) => sum + num, 0);
+
+      }
+
+      private validateNegativeNumber(sanitaizeNumber: number[]): number[] {
+        const negativeNumbers = sanitaizeNumber.filter(num => num < 0);
+        if (negativeNumbers.length) {
+          throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`);
+        }
+        return sanitaizeNumber.filter(num => num > 0);
+      }
+
+      private extractDelimiter(input:string): RegExp{
+        if (input.startsWith("//")) {
+          const parts = input.split("\n");
+          return new RegExp(parts[0].substring(2));
+        }
+        else return /,|\n/
+      }
+
+      private extractNumber(numbers: string, delimiter:RegExp):number[]{
+        if(numbers.startsWith('//')){
+          numbers = numbers.split('\n')[1];
+        }
+
         const numArray = numbers.split(delimiter).map((num) => num.trim()) 
         .filter((num) => num) // Remove empty values
         .map((num) => Number(num)) // Convert to numbers
         .filter((num) => !isNaN(num)); //Remove invalid values
-
-        
-
-        //filter negative number 
-        const negativeNumbers = numArray.filter(num => num < 0);
-        //throw error if any negative input is send
-        if (negativeNumbers.length) {
-            throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`);
-          }
-    
-        return numArray.reduce((sum, num) => sum + num, 0);
+        return numArray;
       }
+  
 }
